f30fd207069300fad74d680f3fd8d4995b7d35d3
Anthony Anderson <anthonyia715@gmail.com>
2024-07-07 18:56:39 -0400

    Adding setup and commands
    Adding typescript setup, base commands, and some code from previous bot just for reference

---
+++ commands/index.ts
@@ -0,0 +1,6 @@
+import * as ping from "./ping";
+import * as supabase from "./supabase";
+
+export const commands = {
+  ping
+};
---
+++ config.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID, SUPABASE_URL, SUPABASE_KEY } = process.env;
+
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !GUILD_ID || !SUPABASE_URL || ! SUPABASE_KEY) {
+  throw new Error("Missing environment variables");
+}
+
+export const config = {
+  DISCORD_TOKEN,
+  DISCORD_CLIENT_ID,
+  GUILD_ID, 
+  SUPABASE_URL, 
+  SUPABASE_KEY
+};
