b5c5a572756b8ce76fb06a465e04eee509259d61
ruuffian <ruuffian@gmail.com>
2024-08-23 16:53:08 -0400

    Added support for PM2 to start the instance, in anticipation of cron process. Also removed dist from source control, and pointed start script to the compressed, minified version of source.

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [
+    {
+      name   : "bot",
+      script : "./dist/index.js"
+    },
+    {
+      name   : "cron",
+      script : "./dist/cron.js"
+    }
+  ]
+}
