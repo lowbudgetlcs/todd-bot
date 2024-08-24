266af699014b07d1add5efe9e3f4c50376598a4c
ruuffian <ruuffian@gmail.com>
2024-08-24 13:01:00 -0400

    Finished code pruning cron job + introspected database

---
+++ src/cron.ts
@@ -0,0 +1,57 @@
+import { Cron } from 'croner';
+import { config } from './config';
+import { db } from './db/db';
+import { games } from './db/schema';
+import { and, eq, sql } from 'drizzle-orm';
+
+type Game = {
+  id: number;
+  short_code: string;
+  winner_id: number | null;
+  loser_id: number | null;
+  series_id: number;
+  result_id: number | null;
+  game_num: number;
+  processed: boolean;
+  created_at: string | null;
+}
+
+// Code pruning job
+const job = Cron("45 23 * * *", async () => {
+  console.info("Begininng pruning...")
+  const unprocessedCodes = await db.select().from(games).where(
+    and(
+      eq(games.processed, false),
+      sql`${games.created_at} < current_timestamp - interval '12 minutes'`
+    )
+  );
+  console.info(`Fetched ${unprocessedCodes.length} codes.`)
+
+  const markGame = async (game: Game) => {
+    const shortCode = game.short_code;
+    try {
+      await config.rAPI.tournamentV5.getTournamentGameDetailsByTournamentCode({ tournamentCode: shortCode });
+    } catch (e: any) {
+      if (e.status === 404) {
+        return shortCode;
+      }
+    }
+    return ""
+  }
+  const markedPromises = unprocessedCodes.map(markGame);
+  const markedCodes = (await Promise.all(markedPromises)).filter(val => val.length > 0);
+  console.info(`Marked ${markedCodes.length} codes for deletion.`)
+
+  const deleteCode = async (code: string) => {
+    try {
+      await db.delete(games).where(eq(games.short_code, code));
+      return 1
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const deletedPromises = markedCodes.map(deleteCode);
+  const deletedCodes = await Promise.all(deletedPromises);
+  const count = deletedCodes.filter(val => typeof val != "undefined").length
+  console.info(`Pruning complete! Deleted ${count} codes.`)
+}).trigger();
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
-import { pgTable, foreignKey, pgEnum, serial, char, integer, boolean, index, varchar, text, type AnyPgColumn, bigint, smallint } from "drizzle-orm/pg-core"
+import { pgTable, foreignKey, unique, pgEnum, serial, integer, bigint, boolean, varchar, timestamp, type AnyPgColumn, index, char, text, uniqueIndex } from "drizzle-orm/pg-core"
   import { sql } from "drizzle-orm"
 
 export const aal_level = pgEnum("aal_level", ['aal1', 'aal2', 'aal3'])
 export const code_challenge_method = pgEnum("code_challenge_method", ['s256', 'plain'])
 export const factor_status = pgEnum("factor_status", ['unverified', 'verified'])
-export const factor_type = pgEnum("factor_type", ['totp', 'webauthn'])
+export const factor_type = pgEnum("factor_type", ['totp', 'webauthn', 'phone'])
 export const one_time_token_type = pgEnum("one_time_token_type", ['confirmation_token', 'reauthentication_token', 'recovery_token', 'email_change_token_new', 'email_change_token_current', 'phone_change_token'])
 export const key_status = pgEnum("key_status", ['default', 'valid', 'invalid', 'expired'])
 export const key_type = pgEnum("key_type", ['aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20'])
@@ -12,96 +12,40 @@ export const action = pgEnum("action", ['INSERT', 'UPDATE', 'DELETE', 'TRUNCATE'
 export const equality_op = pgEnum("equality_op", ['eq', 'neq', 'lt', 'lte', 'gt', 'gte', 'in'])
 
 
-export const accounts = pgTable("accounts", {
-	id: serial("id").primaryKey().notNull(),
-	riot_puuid: char("riot_puuid", { length: 78 }).notNull(),
-	player_id: integer("player_id").notNull().references(() => players.id),
-	is_primary: boolean("is_primary").notNull(),
-});
-
-export const divisions = pgTable("divisions", {
-	id: serial("id").primaryKey().notNull(),
-	name: varchar("name", { length: 20 }),
-	description: text("description"),
-	provider_id: integer("provider_id").notNull(),
-	tournament_id: integer("tournament_id").notNull(),
-	groups: integer("groups"),
-},
-(table) => {
-	return {
-		lower_idx: index("divisions_lower_idx").using("btree", sql`lower((name)::text)`),
-	}
-});
-
-export const performances = pgTable("performances", {
-	id: serial("id").primaryKey().notNull(),
-	player_id: integer("player_id").references(() => players.id),
-	team_id: integer("team_id").references(() => teams.id),
-	game_id: integer("game_id").references(() => games.id),
-});
-
-export const players = pgTable("players", {
-	id: serial("id").primaryKey().notNull(),
-	primary_riot_puuid: char("primary_riot_puuid", { length: 78 }).notNull(),
-	team_id: integer("team_id").references((): AnyPgColumn => teams.id),
-});
-
-export const schedules = pgTable("schedules", {
-	id: serial("id").primaryKey().notNull(),
-	week: integer("week").notNull(),
-	division_id: integer("division_id").notNull().references(() => divisions.id),
-	group_id: char("group_id", { length: 1 }).notNull(),
-	series_id: integer("series_id").notNull().references(() => series.id),
-});
-
-export const results = pgTable("results", {
-	id: serial("id").primaryKey().notNull(),
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	start_time: bigint("start_time", { mode: "number" }),
-	short_code: varchar("short_code", { length: 100 }).notNull(),
-	meta_data: text("meta_data").notNull(),
-	game_id: integer("game_id"),
-	game_name: varchar("game_name", { length: 60 }),
-	game_type: varchar("game_type", { length: 20 }),
-	game_map: varchar("game_map", { length: 20 }),
-	game_mode: varchar("game_mode", { length: 20 }),
-	region: varchar("region", { length: 20 }),
-});
-
 export const series = pgTable("series", {
 	id: serial("id").primaryKey().notNull(),
-	team1_id: integer("team1_id").notNull().references(() => teams.id),
-	team2_id: integer("team2_id").notNull().references(() => teams.id),
+	team1_id: integer("team1_id").references(() => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
+	team2_id: integer("team2_id").references(() => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
 	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
 	message_id: bigint("message_id", { mode: "number" }),
 	playoffs: boolean("playoffs").notNull(),
 	win_condition: integer("win_condition").notNull(),
+	winner_id: integer("winner_id").references(() => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
+},
+(table) => {
+	return {
+		series_team1_id_team2_id_playoffs_key: unique("series_team1_id_team2_id_playoffs_key").on(table.team1_id, table.team2_id, table.playoffs),
+	}
 });
 
 export const games = pgTable("games", {
 	id: serial("id").primaryKey().notNull(),
 	short_code: varchar("short_code", { length: 100 }).notNull(),
-	winner_id: integer("winner_id").references(() => teams.id),
-	loser_id: integer("loser_id").references(() => teams.id),
-	series_id: integer("series_id").notNull().references(() => series.id),
-	result_id: integer("result_id").references(() => results.id),
+	winner_id: integer("winner_id").references(() => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
+	loser_id: integer("loser_id").references(() => teams.id, { onUpdate: "cascade" } ),
+	series_id: integer("series_id").notNull().references(() => series.id, { onDelete: "set null", onUpdate: "cascade" } ),
+	result_id: integer("result_id").references(() => results.id, { onDelete: "set null", onUpdate: "cascade" } ),
 	game_num: integer("game_num").default(0).notNull(),
-});
-
-export const standings = pgTable("standings", {
-	id: serial("id").primaryKey().notNull(),
-	placement: integer("placement").notNull(),
-	division_id: integer("division_id").notNull(),
-	group_id: char("group_id", { length: 1 }).notNull(),
-	team_id: integer("team_id").notNull().references(() => teams.id),
+	processed: boolean("processed").default(false).notNull(),
+	created_at: timestamp("created_at", { mode: 'string' }).defaultNow(),
 });
 
 export const teams = pgTable("teams", {
 	id: serial("id").primaryKey().notNull(),
 	name: varchar("name", { length: 255 }).notNull(),
-	division_id: integer("division_id").notNull().references(() => divisions.id),
+	division_id: integer("division_id").references(() => divisions.id, { onDelete: "set null", onUpdate: "cascade" } ),
 	group_id: char("group_id", { length: 1 }).notNull(),
-	captain_id: integer("captain_id").references((): AnyPgColumn => players.id),
+	captain_id: integer("captain_id").references((): AnyPgColumn => players.id, { onDelete: "set null", onUpdate: "cascade" } ),
 	logo: varchar("logo"),
 },
 (table) => {
@@ -110,12 +54,84 @@ export const teams = pgTable("teams", {
 	}
 });
 
+export const standings = pgTable("standings", {
+	id: serial("id").primaryKey().notNull(),
+	placement: integer("placement").notNull(),
+	division_id: integer("division_id").notNull(),
+	group_id: char("group_id", { length: 1 }).notNull(),
+	team_id: integer("team_id").notNull().references(() => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
+});
+
+export const schedules = pgTable("schedules", {
+	id: serial("id").primaryKey().notNull(),
+	week: integer("week").notNull(),
+	division_id: integer("division_id").notNull().references(() => divisions.id, { onDelete: "set null", onUpdate: "cascade" } ),
+	group_id: char("group_id", { length: 1 }).notNull(),
+	series_id: integer("series_id").notNull().references(() => series.id, { onDelete: "set null", onUpdate: "cascade" } ),
+});
+
+export const divisions = pgTable("divisions", {
+	id: serial("id").primaryKey().notNull(),
+	name: varchar("name", { length: 20 }).notNull(),
+	description: text("description"),
+	provider_id: integer("provider_id").notNull(),
+	tournament_id: integer("tournament_id").notNull(),
+	groups: integer("groups"),
+},
+(table) => {
+	return {
+		lower_idx: index("divisions_lower_idx").using("btree", sql`lower((name)::text)`),
+		divisions_tournament_id_key: unique("divisions_tournament_id_key").on(table.tournament_id),
+	}
+});
+
+export const players = pgTable("players", {
+	id: serial("id").primaryKey().notNull(),
+	primary_riot_puuid: char("primary_riot_puuid", { length: 78 }).notNull(),
+	team_id: integer("team_id").references((): AnyPgColumn => teams.id, { onDelete: "set null", onUpdate: "cascade" } ),
+	summoner_name: varchar("summoner_name", { length: 40 }),
+},
+(table) => {
+	return {
+		primary_riot_puuid_idx: uniqueIndex("players_primary_riot_puuid_idx").using("btree", table.primary_riot_puuid),
+		players_primary_riot_puuid_key: unique("players_primary_riot_puuid_key").on(table.primary_riot_puuid),
+		players_summoner_name_key: unique("players_summoner_name_key").on(table.summoner_name),
+	}
+});
+
+export const accounts = pgTable("accounts", {
+	id: serial("id").primaryKey().notNull(),
+	riot_puuid: char("riot_puuid", { length: 78 }).notNull(),
+	player_id: integer("player_id").notNull().references(() => players.id, { onDelete: "cascade", onUpdate: "cascade" } ),
+	is_primary: boolean("is_primary").notNull(),
+});
+
 export const group_keys = pgTable("group_keys", {
-	id: smallint("id").primaryKey().notNull(),
+	id: integer("id").primaryKey().notNull(),
 	letter: char("letter", { length: 1 }),
 },
 (table) => {
 	return {
 		lower_idx: index("group_keys_lower_idx").using("btree", sql`lower((letter)::text)`),
+	}
+});
+
+export const results = pgTable("results", {
+	id: serial("id").primaryKey().notNull(),
+	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
+	start_time: bigint("start_time", { mode: "number" }),
+	short_code: varchar("short_code", { length: 100 }).notNull(),
+	meta_data: text("meta_data").notNull(),
+	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
+	game_id: bigint("game_id", { mode: "number" }),
+	game_name: varchar("game_name", { length: 60 }),
+	game_type: varchar("game_type", { length: 20 }),
+	game_map: integer("game_map"),
+	game_mode: varchar("game_mode", { length: 20 }),
+	region: varchar("region", { length: 20 }),
+},
+(table) => {
+	return {
+		uq_short_code: unique("uq_short_code").on(table.short_code),
 	}
 });
